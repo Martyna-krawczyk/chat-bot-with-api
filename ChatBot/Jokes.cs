9e5729d9f3983a81e611ca221805664aaf0fa214
Martyna Krawczyk <martynakraw@gmail.com>
2020-02-21 10:37:24 +1100

    initial commit with returnedf API data

---
+++ ChatBot/Jokes.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace ChatBot
+{
+    public class Jokes
+    {
+        public class Flags
+        {
+            public bool nsfw { get; set; }
+            public bool religious { get; set; }
+            public bool political { get; set; }
+            public bool racist { get; set; }
+            public bool sexist { get; set; }
+        }
+
+        public class RootObject
+        {
+            public string joke;
+            public string category { get; set; }
+            public string type { get; set; }
+            public int id { get; set; }
+            public bool error { get; set; }
+            public Flags flags { get; set; }
+        }
+
+        public class WebAndJSON
+        {
+            public static string chosenCategory;
+
+            public static string AccessJokes(string chosenCategory)
+            {
+                HttpClient client = new HttpClient();
+
+                var response = client.GetAsync("https://sv443.net/jokeapi/v2/joke/" + chosenCategory + "?type=single").Result;
+
+                
+                return response.Content.ReadAsStringAsync().Result;
+            }
+        }
+
+
+    }
+
+
+}
---
+++ ChatBot/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Newtonsoft.Json;
+using static ChatBot.Jokes;
+
+namespace ChatBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi!");
+
+            Console.WriteLine("What joke category would you like?");
+            string category = Console.ReadLine();
+            ReturnAJoke(category);
+
+            static void ReturnAJoke(string category)
+            {
+                string wordJSON = WebAndJSON.AccessJokes(category);
+
+                RootObject APIResults = JsonConvert.DeserializeObject<RootObject>(wordJSON);
+
+                Console.WriteLine(APIResults.joke);
+            }
+        }
+    }
+}
